5bb02a791424ae872d09fd5074106cc6c3de0d3f
fgonzale <francois.gnz@gmail.com>
2022-11-25 23:07:42 +0100

    update

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,60 +6,65 @@
 /*   By: francois <francois@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 15:43:29 by francois          #+#    #+#             */
-/*   Updated: 2022/11/24 19:28:29 by francois         ###   ########.fr       */
+/*   Updated: 2022/11/25 23:03:28 by francois         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 #include "./libft/libft.h"
-#include <stdio.h>
+
+int	ft_format(va_list ap, const char format)
+{
+	int	len;
+
+	len = 0;
+	if (format == 's')
+		len += ft_putstrlen(va_arg(ap, char *));
+	if (format == 'c')
+		len += ft_putcharlen(va_arg(ap, int));
+	if (format == 'd')
+		len += ft_putnbrlen(va_arg(ap, int));
+	if (format == '%')
+	{
+		write (1, "%", 1);
+		len += 1;
+	}
+	return (len);
+}
 
 int	ft_printf(const char *str, ...)
 {
     int	i;
+	int	lentotal;
 	va_list	ap;
 	char	*str_arg;
 
 	i = 0;
+	lentotal = 0;
 	va_start(ap, str);
 	while (str[i] != '\0')
 	{
 		if (str[i] == '%')
 		{
+			lentotal += ft_format(ap, str[i + 1]);
 			i++;
-			if (str[i] == 's')
-			{
-				str_arg = va_arg(ap, char *);
-				ft_putstr_fd(str_arg, 1);
-				i++;
-			}
-			else if (str[i] == 'c')
-			{
-				ft_putchar_fd(va_arg(ap, int), 1);
-				i++;
-			}
-			else if(str[i] == 'd')
-			{
-				ft_putnbr_fd(va_arg(ap, int), 1);
-				i++;
-			}
-			else if(str[i] == '%')
-			{
-				ft_putchar_fd('%', 1);
-				i++;
-			}
 		}
 		else
-		{
-			ft_putchar_fd(str[i], 1);
-			i++;
-		}
-	
+			lentotal += ft_putcharlen(str[i]);
+		i++;
 	}
 	va_end(ap);
-	return (i);
+	return (lentotal);
 }
 int	main()
 {
-	ft_printf("J'ai %d %s et %d %s\n", 100, "euros", -150, "dollars");
+	void	*ptr;
+	char	*chaine;
+	int	i;
+
+	
+	chaine = (char *)ptr;
+	chaine = "SALut";
+	i = ft_printf("");
+	printf("%s", "");
 }
--- ft_printf/ft_utils.c
+++ ft_printf/ft_utils.c
@@ -1,27 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*   ft_utils.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: francois <francois@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/24 15:39:39 by francois          #+#    #+#             */
-/*   Updated: 2022/11/25 22:30:29 by francois         ###   ########.fr       */
+/*   Created: 2022/11/25 20:38:14 by francois          #+#    #+#             */
+/*   Updated: 2022/11/25 23:05:58 by francois         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_PRINTF_H
-# define LIBFT_PRINTF_H
+#include "./libft/libft.h"
+#include "libftprintf.h"
 
-# include <unistd.h>
-# include <stdlib.h>
-# include <stdarg.h>
-# include <stdio.h>
 
-int	ft_printf(const char *, ...);
-int	ft_formats(va_list ap, const char format);
-int	ft_putstrlen(char *str);
-int	ft_putcharlen(int c);
-int	ft_putnbrlen(int nb);
+int	ft_putstrlen(char *str)
+{
+	int	i;
 
-#endif
+	i = 0;
+	while (str[i])
+	{
+		write (1, &str[i], 1);
+		i++;
+	}
+	return (i);
+}
+
+int	ft_putcharlen(int c)
+{
+	write (1, &c, 1);
+	return (1);
+}
+
+int	ft_putnbrlen(int nb)
+{
+	char	*temp;
+	int	len;
+
+	temp = ft_itoa(nb);
+	len = ft_putstrlen(temp);
+	free(temp);
+	return (len);
+}
